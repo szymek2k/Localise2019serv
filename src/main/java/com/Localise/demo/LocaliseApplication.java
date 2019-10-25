647e9da570bcdf5d49c936fdf6b8c289d4d1199a
MarcinSz <marcin@o2.pl>
2019-10-25 22:28:46 +0200

    feat(init) Init spring boot project

---
+++ src/main/java/com/Localise/demo/LocaliseApplication.java
@@ -0,0 +1,13 @@
+package com.Localise.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaliseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaliseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Localise/demo/LocaliseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Localise.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaliseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
